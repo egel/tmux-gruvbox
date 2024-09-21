ebbdcb187fab809e2a284873daffda810384ec1c
Maciej Sypien <maciejsypien@gmail.com>
2024-09-21 10:50:10 +0200

    fix: update exec of multiple scripts for linux

--- tests/run_all_linux_tests.sh
+++ tests/run_all_linux_tests.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 main() {
-  for test in $(compgen -A function | grep "^test_linux_"); do "$test"; done
+  set -e # exit on error
+  _files=$(find "$CURRENT_DIR" -name "test_linux_*" -type f)
+  for test in $_files; do
+    bash -c "$test"
+  done
 }
 
 main "$@"
